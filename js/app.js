3119b2862c95740c0a620f9d20bdf1e4c4be6a7f
Dywrench <jv88611@gmail.com>
2025-09-02 17:32:16 -0500

    version 1.0
    se agrego el código HTML incluyendo 5 etiquetas, lógica a los botones y estilos

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function mostrarReferencia() {
+    alert("Referencia en formato APA:\n\nKarunamurthy, A., Vijayan, K., Kshirsagar, P. R., et al. (2025). An optimal federated learning-based intrusion detection for IoT environment. Scientific Reports, 15, 8696. https://doi.org/10.1038/s41598-025-93501-8");
+}
+
+
+function cambiarFondo() {
+    // cambia el color aleatoriamente entre los colores pastel (asi se puede leer mejor)
+    let colores = ["#FFDDC1", "#C1FFD7", "#C1E1FF", "#F7C1FF", "#FFF7C1"];
+    let aleatorio = colores[Math.floor(Math.random() * colores.length)];
+    document.body.style.backgroundColor = aleatorio;
+}
+
+
+function mostrarMensaje() {
+    alert("Bienvenido/a nuestro trabajo de consulta base de datos");
+}
